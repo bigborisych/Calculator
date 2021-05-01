5c61e4d3478a48133549074238576a3ac34b6349
Borisov Vlad <borisov.vlad27@gmail.com>
2021-05-01 16:31:37 +0300

    edited getNum method in RomeNum.java

--- src/ru/borisych/RomeNum.java
+++ src/ru/borisych/RomeNum.java
@@ -20,8 +20,10 @@ public class RomeNum implements Num {
     @Override
     public int getNum() {
         String[] romeChars = romeChar.split(" ");
-        for (int i = 0; romeChars.length <= 9; i++) {
-            if (number.equals(romeChars[i])) return i + 1;
+        for (int i = 0; i <= 9; i++) {
+            if (number.equals(romeChars[i])) {
+                return i + 1;
+            }
         }
         return 0;
     }
